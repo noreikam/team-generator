f0351b6e06c4fad30b036db9efbf9a856cf80b3b
noreikam <noreikam1@gmail.com>
2022-05-23 14:26:51 -0400

    cleaned up file structure and console logs

--- index.js
+++ index.js
@@ -1,9 +1,141 @@
-const promptUser = require('./utils/promptUser');
-const generateHTML = require('./src/html-template');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const htmlTemplate = require('./src/html-template');
+const { writeFile, copyFile } = require('./utils/generate-files');
+
+const team = [];
+
+const addEngineer = () => {
+  return inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'name', 
+      message: 'Enter engineer name'
+    }, 
+    {
+      type: 'input', 
+      name: 'id', 
+      message: 'Enter engineer id'
+    }, 
+    {
+      type: 'input', 
+      name: 'email', 
+      message: 'Enter engineer email'
+    }, 
+    {
+      type: 'input', 
+      name: 'github', 
+      message: 'Enter engineer github'
+    }
+  ])
+  .then(engineerData => {
+    const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github);
+    team.push(engineer);
+    return promptTeam();
+  })
+}
+
+const addIntern = () => {
+  return inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'name', 
+      message: 'Enter intern name'
+    }, 
+    {
+      type: 'input', 
+      name: 'id', 
+      message: 'Enter intern id'
+    }, 
+    {
+      type: 'input', 
+      name: 'email', 
+      message: 'Enter intern email'
+    }, 
+    {
+      type: 'input', 
+      name: 'school', 
+      message: 'Enter intern school'
+    }
+  ])
+  .then(internData => {
+    const intern = new Intern(internData.name, internData.id, internData.email, internData.school);
+    team.push(intern);
+    return promptTeam();
+  })
+}
+
+const promptTeam = () => {
+  return inquirer.prompt(
+    {
+      type: 'list', 
+      message: 'What would you like to do?',
+      name: 'action',
+      choices: ['Add Engineer', 'Add Intern', 'Complete Team']
+    }
+  )
+  .then((response) => {
+    switch (response.action) {
+      case 'Add Engineer': addEngineer();
+        break;
+      case 'Add Intern': addIntern();
+        break;
+      case 'Complete Team':
+        // return team;
+        writeFile(htmlTemplate(team));
+        copyFile();
+        break;
+    }
+  } )
+}
 
 
-promptUser()
-  // .then(teamData => {
-  //   return generateHTML(teamData);
+const promptManager = () => {
+  return inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'name', 
+      message: 'Enter manager name'
+    }, 
+    {
+      type: 'input', 
+      name: 'id', 
+      message: 'Enter manager id'
+    }, 
+    {
+      type: 'input', 
+      name: 'email', 
+      message: 'Enter manager email'
+    }, 
+    {
+      type: 'input', 
+      name: 'officeNumber', 
+      message: 'Enter manager office number'
+    }
+  ])
+  .then(managerData => {
+    const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
+    team.push(manager);
+    return promptTeam();
+  })
+}
+
+promptManager()
+  // .then(team => {
+  //   return htmlTemplate(team);
   // })
-
+  // .then(pageHTML => {
+  //   return writeFile(pageHTML);
+  // })
+  // .then(writeFileResponse => {
+  //   console.log(writeFileResponse);
+  //   return copyFile();
+  // })
+  // .then(copyFileResponse => {
+  //   console.log(copyFileResponse);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
--- src/html-template.js
+++ src/html-template.js
@@ -1,16 +1,70 @@
+const generateRowThree = (officeNumber, github, school) => {
+  if(officeNumber) {
+    return `Office Number: ${officeNumber}`
+  }
+  else if (github) {
+    return `Github: <a href="https://github.com/${github}" target="_blank">${github}</a>`
+  } else {
+    return `School: ${school}`
+  }
+}
+
+const generateRole = role => {
+  if (role === 'Manager') {
+    return `<i class="fa-solid fa-mug-hot"></i> Manager`
+  } else if (role === 'Engineer') {
+    return `<i class="fa-solid fa-glasses"></i> Engineer`
+  } else {
+    return `<i class="fa-solid fa-user-graduate"></i> Intern`
+  }
+}
+
+const generateTeam = teamData => {
+  return `${teamData.map(({ name, id, email, role, officeNumber, github, school }) => {
+    return ` 
+      <article>
+        <div class="card-header">
+          <h3>${name}</h3>
+          <h3>${generateRole(role)}</h3>
+        </div>
+        <div class="card-body">
+          <h6>ID: ${id}</h6>
+          <h6> Email: <a href="mailto: ${email}">${email}</a></h6>
+          <h6>${generateRowThree(officeNumber, github, school)}</h6>
+        </div>
+      </article>
+    `; 
+    }).join('')
+  }`
+}
 
 
 const generateHTML = teamData => {
-  // console.log('generateHTML');
-  // console.log(teamData);
-  // for (let i = 0; i < teamData.length; i++) {
-  //   console.log(teamData[i].name);
-  //   console.log(teamData[i].id);
-  //   console.log(teamData[i].email);
-  //   console.log(teamData[i].role);
-  //   console.log(teamData[i].officeNumber);
-  // }
-  
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My Team</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+</head>
+
+<body>
+  <header>
+    <div>
+      <h1>My Team</h1>
+    </div>
+  </header>
+
+  <main>
+    ${generateTeam(teamData)}
+  </main>
+</body>
+
+</html>`
 }
 
 module.exports = generateHTML
